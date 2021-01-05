21fb3d077200f359c077cc2f558bee549ce84944
mcdull76 <im@randyyeung.com>
2021-01-05 22:07:07 +0800

    Update autoScale.js

--- autoScale.js
+++ autoScale.js
@@ -1,4 +1,6 @@
 let global_scaling = 1;
+const defaultW = 1480+250;
+const defaultH = 1080;
 
 window.onresize = autoScale;
 
@@ -6,8 +8,6 @@ function autoScale( ) {
 	const split = window.location.href.split("scale=");
 	let scale = parseFloat(split[1]);
 
-	const defaultW = 1480+250;
-	const defaultH = 1080;
 
 	const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
 	const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
